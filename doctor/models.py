4a6c8ce22e75fb9407c88772a48851696751b740
Beknaz <choybekov.beknaz@gmail.com>
2022-08-22 12:10:16 +0600

    ServiceListing

--- doctor/models.py
+++ doctor/models.py
@@ -6,7 +6,7 @@ class Category(models.Model):
     title = models.CharField(max_length=100)
 
 
-class Service_listing(models.Model):
+class ServiceListing(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -36,7 +36,7 @@ class Doctor(models.Model):
 class Entry(models.Model):
     doctor = models.ForeignKey(Doctor, related_name='entrys', on_delete=models.CASCADE)
     user = models.ForeignKey(User, related_name='entrys', on_delete=models.CASCADE)
-    service_listing = models.ForeignKey(Service_listing, related_name='entrys', on_delete=models.CASCADE)
+    service_listing = models.ForeignKey(ServiceListing, related_name='entrys', on_delete=models.CASCADE)
     booking_datetime = models.DateTimeField(auto_now_add=True)
     arrival_datetime = models.DateTimeField()
     departure_datetime = models.DateTimeField()
